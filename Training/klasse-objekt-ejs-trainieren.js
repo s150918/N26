7a78e1279054e8c61ff7d5fde0081a67ffb8ff88
s150918 <s150918@berufskolleg-borken.de>
2019-04-29 14:24:17 +0200

    ...

--- Training/klasse-objekt-ejs-trainieren.js
+++ Training/klasse-objekt-ejs-trainieren.js
@@ -5,23 +5,72 @@ class Auto {
     }
 }
 
-class Schueler{
+
+// eine Klasse ist ein Bauplan. Der Bauplan sieht vor, wie Objekte erstellt werden. 
+// alle Objekte, die von einem Bauplan erstellt werden, haben dieselben Eigenschaften, aber möglicherweise unterschiedliche Eigenschaftswerte
+// Klassendefinition
+
+ class Rechteck{
+     constructor(){
+         this.laenge
+         this.breite
+     }
+}
+
+// Klassendefinition für Schüler in einer Schule:
+
+class Schueler {
     constructor(){
-        this.Vorname
-        this.Nachname
-        this.Alter
+        this.geschlecht
+        this.klasse
+        this.alter
+        this.vorname
+        this.nachname
     }
 }
 
-class Konto{
+class Fussballer {
     constructor(){
-        this.Kontonummer
-        this.Kontoart
+        this.vorname
+        this.nachname
+        this.verein
+        this.vertragslaufzeit
+        this.marktwert
     }
 }
 
 
 
+
+    //Deklaration eines neuen Objekts vom Typ Rechteck
+    // Deklaration = Bekanntmachung
+    //let rechteck = ...
+    // Instanzierung eines neuen Objektes
+    // Instanzierung erkennt man immer am reservierten Wort new
+    // bei der Instanzierung wird Arbeitsspeicher bereitgestellt. 
+    // ... = new Rechteck()
+    
+// 1. Deklaration  2. Instanzierung  
+    let rechteck = new Rechteck()
+    let schueler = new Schueler()
+    let fussballer = new Fussballer
+   
+    // 3. Initalisierung (konkrete Eigenschaftswerte werden zugewiesen)
+   
+    rechteck.breite = 2
+    rechteck.laenge = 3 
+
+    schueler.geschlecht = "m"
+    schueler.alter = 17
+
+    fussballer.vorname = "Ahmed"
+    fussballer.nachname = "Kutucu"
+    fussballer.verein = "FC Schalke 04"
+    fussballer.vertragslaufzeit = 3
+    fussballer.marktwert = 10
+
+ console.log("Länge:" +rechteck.laenge)
+console.log("breite:" +rechteck.breite)
 
 const express = require('express')
 const bodyParser = require('body-parser')
@@ -40,6 +89,15 @@ app.get('/klasse-objekt-ejs-trainieren',(req, res, next) => {
 
     // ... wird klasse-objekt-ejs-trainieren.ejs gerendert:
 
-    res.render('klasse-objekt-ejs-trainieren', {                                      
+    res.render('klasse-objekt-ejs-trainieren', {   
+        breite : rechteck.breite,   
+        laenge : rechteck.laenge,        
+        geschlecht : schueler.geschlecht,
+        alter : schueler.alter,
+        vorname : fussballer.vorname,
+        nachname : fussballer.nachname,
+        verein : fussballer.verein,
+        vertragslaufzeit : fussballer.vertragslaufzeit,  
+        marktwert : fussballer.marktwert                   
     })
 })
